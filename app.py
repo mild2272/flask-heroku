aa35f564c7d8d45d4c4e6213ed65d7d2cf5b2f29
mild2272 <89238975+mild2272@users.noreply.github.com>
2021-09-03 12:02:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,9 @@ def hello():
 def get_api():
     return jsonify(data)
 
+@app.route(',/,')
+def hi():
+    return "สวัสดีค้าบบบ"    
+        
 if __name__ == "__main__":
     app.run(debug=False)
